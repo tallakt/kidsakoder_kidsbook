33022f917b6f23304eb8e815d029ad30683de3d1
Tallak Tveide <tallak@tveide.net>
2015-02-08 23:07:26 +0100

    Kontiki funker nesten helt ok

---
+++ kode/kontiki/config.ru
@@ -0,0 +1,2 @@
+require './kontiki'
+run Sinatra::Application
---
+++ kode/kontiki/views/forsvunnet.rb
@@ -0,0 +1,7 @@
+<h2>Forsvunnet</h2>
+<p>Vi har drevet så langt fra ruten vår at vi blir fanget opp av en
+tidevannsstrøm som tar oss langt bort. Det umulig å komme seg til målet nå.</p>
+
+<p>
+  <a href="/">Prøv på nytt</a>
+</p>
